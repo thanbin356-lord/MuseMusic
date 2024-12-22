03fed5df55a3c4539ccc5b1d6566fd7c28238cad
HwangInu <hoanginu1607@gmail.com>
2024-12-22 13:30:28 +0700

    Hoang update MusicMood

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -39,6 +39,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult MusicMood()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
