979946ce0036532a4375a1c932023c4e6851d1c6
HwangInu <hoanginu1607@gmail.com>
2025-01-07 00:06:34 +0700

    add Imageurl

--- Generate-model.sh
+++ Generate-model.sh
@@ -1,1 +1,1 @@
-dotnet ef dbcontext scaffold "Server=localhost;Database=shopmanagement;User=root;Password=thuthu121;Allow User Variables=True;" Pomelo.EntityFrameworkCore.MySql -o Models/Tables -f
+dotnet ef dbcontext scaffold "Server=localhost;Database=shopmanagement;User=root;Password=hoang123;Allow User Variables=True;" Pomelo.EntityFrameworkCore.MySql -o Models/Tables -f
